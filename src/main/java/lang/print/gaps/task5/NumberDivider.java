aa26b3f876f9fcb5708859197e35b50ffb03f64e
yuldashevmuhiddin65 <102411263+yuldashevmuhiddin65@users.noreply.github.com>
2023-05-20 19:38:50 +0500

    done!

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,8 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    void divide(int toBeDivided) {
+    void divide() {
+        double result = (double) 19 / 5;
+        System.out.println(result);
     }
 }
